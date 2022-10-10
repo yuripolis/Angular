f28c6bd8a402519b78d788debe9400cac1632919
yuripolis <yuripolistchuk@gmail.com>
2022-10-10 13:53:24 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Holder } from './model/holder';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  title = 'AngularHttpRequest';
+
+  holders: Holder[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+
+  
+    this.http.get<Holder[]>("https://jsonplaceholder.typicode.com/posts").subscribe(dados => this.holders = dados);
+  
+  }
+
+
+  onProductCreate(products: {pName: string, desc: string, price: string}) {
+    console.log(products);
+    this.http.post("https://jsonplaceholder.typicode.com/posts", products)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+}
+
+
+
+
+
